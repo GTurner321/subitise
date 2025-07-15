08734954c24925337966192058092596d77b51fb
GTurner321 <george.turner@jumeirahcollege.com>
2025-07-15 12:59:32 +0400

    Update twodicegamecontroller.js

--- games/twodice/js/twodicegamecontroller.js
+++ games/twodice/js/twodicegamecontroller.js
@@ -4,11 +4,6 @@ class TwoDiceGameController {
         this.rainbow = new Rainbow();
         this.bear = new Bear();
         
-        // Initialize dice tester (optional - only if class is available)
-        if (typeof DiceTransitionTester !== 'undefined') {
-            this.diceTester = new DiceTransitionTester(this.diceRenderer);
-        }
-        
         // Game state
         this.questionsCompleted = 0;
         this.gameComplete = false;
