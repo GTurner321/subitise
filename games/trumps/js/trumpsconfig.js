556a04f5be046021dd0d578505c2ca72b13a10d2
GTurner321 <george.turner@jumeirahcollege.com>
2025-07-24 15:45:07 +0400

    Update trumpsconfig.js

--- games/trumps/js/trumpsconfig.js
+++ games/trumps/js/trumpsconfig.js
@@ -15,14 +15,14 @@ const CONFIG = {
     
     // Categories
     CATEGORIES: {
-        FUNNY: 'funny',
+        FUN: 'fun',
         CUDDLY: 'cuddly', 
         STARS: 'stars'
     },
     
     // Category display info
     CATEGORY_INFO: {
-        funny: { label: 'Funny', suffix: '%', max: 100 },
+        fun: { label: 'Fun', suffix: '%', max: 100 },
         cuddly: { label: 'Cuddly', suffix: '/10', max: 10 },
         stars: { label: 'Stars', suffix: '', max: 5 } // No suffix for star display
     },
@@ -40,97 +40,97 @@ const CONFIG = {
             id: 1,
             name: "Biscuit Bear",
             image: "../../assets/trumps/biscuitbear.png",
-            stats: { funny: 24, cuddly: 3, stars: 1.5 }
+            stats: { fun: 45, cuddly: 3, stars: 1.5 }
         },
         {
             id: 2,
             name: "Black Bear",
             image: "../../assets/trumps/blackbear.png", 
-            stats: { funny: 42, cuddly: 7, stars: 3.5 }
+            stats: { fun: 42, cuddly: 7, stars: 3.5 }
         },
         {
             id: 3,
             name: "Casper Rabbit",
             image: "../../assets/trumps/casperrabbit.png",
-            stats: { funny: 31, cuddly: 7, stars: 3.5 }
+            stats: { fun: 31, cuddly: 7, stars: 3 }
         },
         {
             id: 4,
             name: "Chick",
             image: "../../assets/trumps/chick.png",
-            stats: { funny: 40, cuddly: 6, stars: 3 }
+            stats: { fun: 40, cuddly: 6, stars: 3 }
         },
         {
             id: 5,
             name: "Dinosaur",
             image: "../../assets/trumps/dinosaur.png",
-            stats: { funny: 54, cuddly: 5, stars: 4.5 }
+            stats: { fun: 70, cuddly: 5, stars: 4.5 }
         },
         {
             id: 6,
             name: "Elephant",
             image: "../../assets/trumps/elephant.png",
-            stats: { funny: 15, cuddly: 8, stars: 1 }
+            stats: { fun: 15, cuddly: 9, stars: 1 }
         },
         {
             id: 7,
             name: "Flabber Jabber",
             image: "../../assets/trumps/flabberjabber.png",
-            stats: { funny: 98, cuddly: 4, stars: 5 }
+            stats: { fun: 98, cuddly: 4, stars: 5 }
         },
         {
             id: 8,
             name: "Bear",
             image: "../../assets/trumps/gemsbear.png",
-            stats: { funny: 57, cuddly: 8, stars: 4 }
+            stats: { fun: 57, cuddly: 8, stars: 3.5 }
         },
         {
             id: 9,
             name: "Knight Bear",
             image: "../../assets/trumps/knightbear.png",
-            stats: { funny: 50, cuddly: 6, stars: 4.5 }
+            stats: { fun: 67, cuddly: 6, stars: 4 }
         },
         {
             id: 10,
             name: "Duck",
             image: "../../assets/trumps/legoduck.png",
-            stats: { funny: 48, cuddly: 1, stars: 2.5 }
+            stats: { fun: 68, cuddly: 1, stars: 2.5 }
         },
         {
             id: 11,
             name: "Penguin",
             image: "../../assets/trumps/penguin.png",
-            stats: { funny: 49, cuddly: 6, stars: 2.5 }
+            stats: { fun: 49, cuddly: 6, stars: 2.5 }
         },
         {
             id: 12,
             name: "Inflatable Sheep",
             image: "../../assets/trumps/sheep.png",
-            stats: { funny: 75, cuddly: 2, stars: 3 }
+            stats: { fun: 86, cuddly: 2, stars: 3 }
         },
         {
             id: 13,
             name: "Soft Rabbit",
             image: "../../assets/trumps/softrabbit.png",
-            stats: { funny: 16, cuddly: 9, stars: 2 }
+            stats: { fun: 16, cuddly: 10, stars: 2 }
         },
         {
             id: 14,
             name: "The Vowel Family",
             image: "../../assets/trumps/vowels.png",
-            stats: { funny: 72, cuddly: 1, stars: 4 }
+            stats: { fun: 72, cuddly: 1, stars: 4 }
         },
         {
             id: 15,
             name: "Guinea Pig",
             image: "../../assets/raisin/guineapig1.png",
-            stats: { funny: 80, cuddly: 7, stars: 5 }
+            stats: { fun: 80, cuddly: 7, stars: 4.5 }
         },
         {
             id: 16,
             name: "Raffles",
             image: "../../assets/bear.png",
-            stats: { funny: 65, cuddly: 9, stars: 5 }
+            stats: { fun: 65, cuddly: 8, stars: 5 }
         }
     ]
 };
