d24df98a4a719793b2232d8b4386a4989436d6bd
GTurner321 <george.turner@jumeirahcollege.com>
2025-08-08 17:18:58 +0400

    Update twodiceconfig.js

--- games/twodice/js/twodiceconfig.js
+++ games/twodice/js/twodiceconfig.js
@@ -33,6 +33,198 @@ const CONFIG = {
     // Audio settings
     AUDIO_ENABLED: true,
     
+    // Educational level system
+    DIFFICULTY_LEVELS: {
+        1: { 
+            name: "Level 1", 
+            diceRange: [1, 2, 3], 
+            possibleSums: [2, 3, 4, 5, 6],
+            description: "Easy sums with small numbers"
+        },
+        2: { 
+            name: "Level 2", 
+            diceRange: [2, 3, 4], 
+            possibleSums: [4, 5, 6, 7, 8],
+            description: "Medium sums with mid-range numbers"
+        },
+        3: { 
+            name: "Level 3", 
+            diceRange: [1, 2, 3, 4, 5, 6], 
+            possibleSums: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            description: "All possible sums with full dice range"
+        }
+    },
+    
+    // Diagonal transform moves and their inverses
+    TRANSFORM_MOVES: {
+        'down-right': { rotX: 90, rotY: 90, inverse: 'up-left' },
+        'up-right': { rotX: -90, rotY: 90, inverse: 'down-left' },
+        'down-left': { rotX: 90, rotY: -90, inverse: 'up-right' },
+        'up-left': { rotX: -90, rotY: -90, inverse: 'down-right' }
+    },
+    
+    // Pre-computed dice state transitions for target-driven rolling
+    // Format: "frontFace,rotation": { "move": { frontFace: newFace, rotation: newRotation } }
+    // frontFace: 1-6 (which face is toward viewer)
+    // rotation: 0, 90, 180, 270 (rotation of that face)
+    DICE_STATE_TRANSITIONS: {
+        // Face 1 (front) orientations
+        "1,0": {
+            "down-right": { frontFace: 3, rotation: 90 },
+            "up-right": { frontFace: 4, rotation: 270 },
+            "down-left": { frontFace: 4, rotation: 90 },
+            "up-left": { frontFace: 3, rotation: 270 }
+        },
+        "1,90": {
+            "down-right": { frontFace: 2, rotation: 90 },
+            "up-right": { frontFace: 3, rotation: 0 },
+            "down-left": { frontFace: 5, rotation: 180 },
+            "up-left": { frontFace: 4, rotation: 0 }
+        },
+        "1,180": {
+            "down-right": { frontFace: 4, rotation: 90 },
+            "up-right": { frontFace: 3, rotation: 270 },
+            "down-left": { frontFace: 3, rotation: 90 },
+            "up-left": { frontFace: 4, rotation: 270 }
+        },
+        "1,270": {
+            "down-right": { frontFace: 5, rotation: 0 },
+            "up-right": { frontFace: 4, rotation: 180 },
+            "down-left": { frontFace: 2, rotation: 270 },
+            "up-left": { frontFace: 3, rotation: 180 }
+        },
+        
+        // Face 2 (right) orientations
+        "2,0": {
+            "down-right": { frontFace: 3, rotation: 0 },
+            "up-right": { frontFace: 4, rotation: 0 },
+            "down-left": { frontFace: 1, rotation: 270 },
+            "up-left": { frontFace: 6, rotation: 90 }
+        },
+        "2,90": {
+            "down-right": { frontFace: 6, rotation: 90 },
+            "up-right": { frontFace: 1, rotation: 270 },
+            "down-left": { frontFace: 3, rotation: 0 },
+            "up-left": { frontFace: 4, rotation: 0 }
+        },
+        "2,180": {
+            "down-right": { frontFace: 4, rotation: 180 },
+            "up-right": { frontFace: 3, rotation: 180 },
+            "down-left": { frontFace: 6, rotation: 90 },
+            "up-left": { frontFace: 1, rotation: 270 }
+        },
+        "2,270": {
+            "down-right": { frontFace: 1, rotation: 90 },
+            "up-right": { frontFace: 6, rotation: 270 },
+            "down-left": { frontFace: 4, rotation: 180 },
+            "up-left": { frontFace: 3, rotation: 180 }
+        },
+        
+        // Face 3 (top) orientations
+        "3,0": {
+            "down-right": { frontFace: 6, rotation: 0 },
+            "up-right": { frontFace: 1, rotation: 0 },
+            "down-left": { frontFace: 2, rotation: 0 },
+            "up-left": { frontFace: 5, rotation: 0 }
+        },
+        "3,90": {
+            "down-right": { frontFace: 5, rotation: 90 },
+            "up-right": { frontFace: 2, rotation: 90 },
+            "down-left": { frontFace: 6, rotation: 90 },
+            "up-left": { frontFace: 1, rotation: 90 }
+        },
+        "3,180": {
+            "down-right": { frontFace: 1, rotation: 180 },
+            "up-right": { frontFace: 6, rotation: 180 },
+            "down-left": { frontFace: 5, rotation: 180 },
+            "up-left": { frontFace: 2, rotation: 180 }
+        },
+        "3,270": {
+            "down-right": { frontFace: 2, rotation: 270 },
+            "up-right": { frontFace: 5, rotation: 270 },
+            "down-left": { frontFace: 1, rotation: 270 },
+            "up-left": { frontFace: 6, rotation: 270 }
+        },
+        
+        // Face 4 (bottom) orientations
+        "4,0": {
+            "down-right": { frontFace: 1, rotation: 180 },
+            "up-right": { frontFace: 6, rotation: 0 },
+            "down-left": { frontFace: 5, rotation: 90 },
+            "up-left": { frontFace: 2, rotation: 270 }
+        },
+        "4,90": {
+            "down-right": { frontFace: 2, rotation: 0 },
+            "up-right": { frontFace: 5, rotation: 180 },
+            "down-left": { frontFace: 1, rotation: 180 },
+            "up-left": { frontFace: 6, rotation: 0 }
+        },
+        "4,180": {
+            "down-right": { frontFace: 6, rotation: 180 },
+            "up-right": { frontFace: 1, rotation: 0 },
+            "down-left": { frontFace: 2, rotation: 90 },
+            "up-left": { frontFace: 5, rotation: 270 }
+        },
+        "4,270": {
+            "down-right": { frontFace: 5, rotation: 180 },
+            "up-right": { frontFace: 2, rotation: 0 },
+            "down-left": { frontFace: 6, rotation: 180 },
+            "up-left": { frontFace: 1, rotation: 0 }
+        },
+        
+        // Face 5 (left) orientations
+        "5,0": {
+            "down-right": { frontFace: 4, rotation: 0 },
+            "up-right": { frontFace: 3, rotation: 0 },
+            "down-left": { frontFace: 6, rotation: 270 },
+            "up-left": { frontFace: 1, rotation: 90 }
+        },
+        "5,90": {
+            "down-right": { frontFace: 1, rotation: 90 },
+            "up-right": { frontFace: 6, rotation: 270 },
+            "down-left": { frontFace: 4, rotation: 0 },
+            "up-left": { frontFace: 3, rotation: 0 }
+        },
+        "5,180": {
+            "down-right": { frontFace: 3, rotation: 180 },
+            "up-right": { frontFace: 4, rotation: 180 },
+            "down-left": { frontFace: 1, rotation: 90 },
+            "up-left": { frontFace: 6, rotation: 270 }
+        },
+        "5,270": {
+            "down-right": { frontFace: 6, rotation: 90 },
+            "up-right": { frontFace: 1, rotation: 270 },
+            "down-left": { frontFace: 3, rotation: 180 },
+            "up-left": { frontFace: 4, rotation: 180 }
+        },
+        
+        // Face 6 (back) orientations
+        "6,0": {
+            "down-right": { frontFace: 4, rotation: 270 },
+            "up-right": { frontFace: 3, rotation: 90 },
+            "down-left": { frontFace: 3, rotation: 270 },
+            "up-left": { frontFace: 4, rotation: 90 }
+        },
+        "6,90": {
+            "down-right": { frontFace: 5, rotation: 180 },
+            "up-right": { frontFace: 4, rotation: 0 },
+            "down-left": { frontFace: 2, rotation: 0 },
+            "up-left": { frontFace: 3, rotation: 180 }
+        },
+        "6,180": {
+            "down-right": { frontFace: 3, rotation: 270 },
+            "up-right": { frontFace: 4, rotation: 90 },
+            "down-left": { frontFace: 4, rotation: 270 },
+            "up-left": { frontFace: 3, rotation: 90 }
+        },
+        "6,270": {
+            "down-right": { frontFace: 2, rotation: 180 },
+            "up-right": { frontFace: 3, rotation: 0 },
+            "down-left": { frontFace: 5, rotation: 0 },
+            "up-left": { frontFace: 4, rotation: 180 }
+        }
+    },
+    
     // Dice face patterns (dots for each number 1-6)
     // FIXED: Swapped patterns for 2 and 5 to match visual dice layout
     DICE_FACES: {
