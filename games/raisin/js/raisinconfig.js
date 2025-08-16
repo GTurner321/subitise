e33a24ebfc61d07aeefbdaa5d3613b0378a50abc
GTurner321 <george.turner@jumeirahcollege.com>
2025-08-16 21:38:07 +0400

    Update raisinconfig.js

--- games/raisin/js/raisinconfig.js
+++ games/raisin/js/raisinconfig.js
@@ -38,41 +38,41 @@ const CONFIG = {
         }
     },
     
+    // Guinea pig sizes (relative to game area height - simpler system)
+    GUINEA_PIG_SIZES: {
+        GP3: 16,  // 16% of game area height
+        GP2: 40,  // 40% of game area height  
+        GP1: 40   // 40% of game area height
+    },
+    
     // Timing configurations
     FLASH_DURATION: 800,
     NEXT_QUESTION_DELAY: 2000,
-    GUINEA_PIG_3_INITIAL_DISPLAY: 4000, // 4 seconds initial display
-    INITIAL_INSTRUCTION_DELAY: 1000, // 1 second after initial display
-    RAISIN_STAGGER_START: 1000, // 1 second after game loads
-    RAISIN_STAGGER_WINDOW: 3000, // 3 second window for raisins to appear
-    RAISIN_FADE_DURATION: 500, // 0.5 seconds fade in
-    GUINEA_PIG_ANIMATION_DURATION: 2000, // 2 seconds for each guinea pig to cross
-    GUINEA_PIG_PAUSE_DURATION: 500, // Pause between guinea pigs
-    
-    // Guinea pig sizes (relative to game area width)
-    GUINEA_PIG_3_SIZE: 0.1, // 10% of game area width
-    GUINEA_PIG_2_SIZE: 0.22, // 22% of game area width (will be made 20% larger)
-    GUINEA_PIG_1_SIZE: 0.22, // 22% of game area width (will be made 20% larger)
+    GUINEA_PIG_3_INITIAL_DISPLAY: 4000,
+    INITIAL_INSTRUCTION_DELAY: 1000,
+    RAISIN_STAGGER_START: 1000,
+    RAISIN_STAGGER_WINDOW: 3000,
+    RAISIN_FADE_DURATION: 500,
+    GUINEA_PIG_ANIMATION_DURATION: 2000,
+    GUINEA_PIG_PAUSE_DURATION: 500,
     
     // Raisin settings
-    RAISIN_SIZE: 0.08, // 8% of game area width
-    RAISIN_MIN_DISTANCE: 0.12, // Minimum distance between raisins
+    RAISIN_SIZE: 0.08,
+    RAISIN_MIN_DISTANCE: 0.12,
     
     // Game area exclusions - area to avoid placing raisins (for Guinea Pig 3)
     GUINEA_PIG_3_EXCLUSION: {
-        x: 0,        // Start from left edge (percentage)
-        y: 0,        // Start from top edge (percentage)
-        width: 0.25, // 25% of game area width
-        height: 0.25 // 25% of game area height
+        x: 0,
+        y: 0,
+        width: 0.25,
+        height: 0.25
     },
     
     // Audio settings
     AUDIO: {
-        // Guinea pig eating sound
         GUINEA_PIG_EATING_SOUND: '../../assets/raisin/guineapigeating.mp3',
-        GUINEA_PIG_SOUND_VOLUME: 0.7, // Loud volume as requested
+        GUINEA_PIG_SOUND_VOLUME: 0.7,
         
-        // Game instructions
         TUTORIAL_MESSAGES: {
             FIRST_QUESTION: 'Watch the hungry guinea pig',
             FIRST_INSTRUCTION: 'There are 5 raisins. The hungry guinea pig is going to eat some of them.',
@@ -109,7 +109,7 @@ const CONFIG = {
     // Button colors for the game
     COLORS: [
         '#ff6b6b', '#4ecdc4', '#45b7d1', '#f9ca24', '#f0932b', 
-        '#eb4d4b', '#6c5ce7', '#a29bfe', '#fd79a8'
+        '#eb4d4b', '#6c5ce7', '#a29bfe', '#fd79a8', '#00b894'
     ],
     
     // Helper functions
@@ -138,7 +138,7 @@ const CONFIG = {
     getHintMessage: function(questionNumber) {
         const hints = this.AUDIO.HINTS;
         return this.isTutorialMode(questionNumber) ? 
-            hints[0] : // "Try counting how many more will make 5"
-            hints[1];  // "Try counting how many more will make 10"
+            hints[0] : 
+            hints[1];
     }
 };
